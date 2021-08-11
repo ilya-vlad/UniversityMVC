64963dfcc9096d83faec5c5e992009e9aa6a3c6f
Ilya Vladelschikov <gogsan95@gmail.com>
2021-08-11 22:47:34 +0500

    added tests

--- Presentation/WebTest/TestsCourse.cs
+++ Presentation/WebTest/TestsCourse.cs
@@ -27,20 +27,94 @@ namespace MVCTest
         }
 
         [Test]
-        public void OpenGroup()
+        public void EditGroup()
         {
-            string expectedUrl = $"https://localhost:44376/courses/{idCourse}/groups/{idGroup}";       
-            
-            var groups = driver.FindElement(groupsButton);
-            groups.Click();
+            By pathName = By.XPath($"//tr[@id='{idGroup}']//td[@name='nameGroup']");
+            By editGroup = By.XPath($"//tr[@id='{idGroup}']//a[text()='Edit']");
+            By pathInputName = By.XPath($"//input[@name='Name']");
+            By pathSubmit = By.XPath($"//input[@type='submit']");
+            string testName = "TestName";
+            bool passed = false;
+
+
+            driver.Navigate().GoToUrl($"https://localhost:44376/courses/{idCourse}/groups");
             Thread.Sleep(500);
-            var students = driver.FindElement(studentsButton);
-            students.Click();
+            var oldNameGroup = driver.FindElement(pathName).Text;
+            var editorGroup = driver.FindElement(editGroup);
+            editorGroup.Click();
+            Thread.Sleep(100);
+            var inputName = driver.FindElement(pathInputName);
+            inputName.Clear();
+            inputName.SendKeys(testName);
+            var submit = driver.FindElement(pathSubmit);
+            submit.Click();            
+            driver.Navigate().GoToUrl($"https://localhost:44376/courses/{idCourse}/groups");
             Thread.Sleep(500);
-            string actualUrl = driver.Url;
-            Assert.AreEqual(expectedUrl, actualUrl);
+            var newNameGroup = driver.FindElement(pathName).Text;
+
+            if (newNameGroup == testName) passed = true;
+
+            driver.FindElement(editGroup).Click();
+            driver.FindElement(pathInputName).Clear();
+            driver.FindElement(pathInputName).SendKeys(oldNameGroup);
+            driver.FindElement(pathSubmit).Click();            
+            Assert.IsTrue(passed);
         }
 
+        [Test]
+        public void EditStudent()
+        {
+            By pathFirstName = By.XPath($"//tr[@id='{idStudent}']//td[@name='FirstName']");
+            By pathLastName = By.XPath($"//tr[@id='{idStudent}']//td[@name='LastName']");
+            By editStudent = By.XPath($"//tr[@id='{idStudent}']//a[text()='Edit']");
+            By pathInputFirstName = By.XPath($"//input[@name='FirstName']");
+            By pathInputLastName = By.XPath($"//input[@name='LastName']");
+            By pathSubmit = By.XPath($"//input[@type='submit']");
+
+            string testFirstName = "Test FirstName";
+            string testLastName = "Test LastName";
+            bool passed = false;
+
+            driver.Navigate().GoToUrl($"https://localhost:44376/courses/{idCourse}/groups/{idGroup}");
+            Thread.Sleep(100);
+            var oldFirstName = driver.FindElement(pathFirstName).Text;
+            var oldLastName = driver.FindElement(pathLastName).Text;
+            var editorStudent = driver.FindElement(editStudent);
+            editorStudent.Click();
+            Thread.Sleep(100);
+
+            var inputFirstName = driver.FindElement(pathInputFirstName);
+            inputFirstName.Clear();
+            inputFirstName.SendKeys(testFirstName);
+
+            var inputLastName = driver.FindElement(pathInputLastName);
+            inputLastName.Clear();
+            inputLastName.SendKeys(testLastName);
+
+            var submit = driver.FindElement(pathSubmit);
+            submit.Click();
+            driver.Navigate().GoToUrl($"https://localhost:44376/courses/{idCourse}/groups/{idGroup}");
+            Thread.Sleep(100);
+            var newFirstName = driver.FindElement(pathFirstName).Text;
+            var newLastName = driver.FindElement(pathLastName).Text;
+            
+            if (newFirstName == testFirstName && newLastName == testLastName) passed = true;
+
+            driver.FindElement(editStudent).Click();
+
+            driver.FindElement(pathInputFirstName).Clear();
+            driver.FindElement(pathInputFirstName).SendKeys(oldFirstName);
+            driver.FindElement(pathInputLastName).Clear();
+            driver.FindElement(pathInputLastName).SendKeys(oldLastName);
+            driver.FindElement(pathSubmit).Click();
+            Assert.IsTrue(passed);           
+        }
+
+        [Test]
+        public void RemoveGroup()
+        {
+
+        }
         [TearDown]
         public void TearDown()
         {
